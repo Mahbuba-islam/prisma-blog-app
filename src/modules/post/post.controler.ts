517bcaadb99ad2c5fef8357fda48e46035f202e5
Mahbuba Islam <mahbubaislam7010@gmail.com>
2026-01-01 14:28:32 -0500

    filter-with-tags

--- src/modules/post/post.controler.ts
+++ src/modules/post/post.controler.ts
@@ -1,26 +1,60 @@
 import type { Request, Response } from "express"
 import { postServices } from "./post.service"
 
+// const getPost = async(req:Request, res:Response) => {
+//  try{
+//    const {search} = req.query
+//    console.log({search});
+     //  const searchString = typeof search === 'string' ? search : undefined
+
+
+//    const tags = req.query.tags ? (req.query.tags as string).split(',') : []
+
+//    const isFeatured = req.query.isFeatured ? 
+//    req.query.isFeatured === 'true' ? true : req.query.isFeatured === 'false' ? false : undefined : undefined
+
+//  
+
+//    const status = req.query.status as postStatus | undefined
+
+
+//    const authorId = req.query.authorId as string | undefined
+
+//    const results = await postServices.getPost({search:searchString, tags, isFeatured, status, authorId})
+//    res.status(200).json(results)
+
+//   }
+//   catch(err){
+//     res.status(400).json({
+//       error:"post getting failed",
+//       details:err
+//     })
+//   }
+ 
+// }
 
 const getPost = async(req:Request, res:Response) => {
- try{
-   const {search} = req.query
-   console.log({search});
-   const tags = req.query.tags ? (req.query.tags as string).split(',') : []
-   const searchString = typeof search === 'string' ? search : undefined
-   const results = await postServices.getPost({search:searchString, tags})
-   res.status(200).json(results)
+  try{
 
+     const search = req.query.search 
+    const searchString = typeof search === 'string' ? search :undefined
+
+    const tags = req.query.tags as string[]
+    
+   const results = await postServices.getPost({search:searchString, tags})
+  
+ 
+
+  res.status(200).json(results)
   }
   catch(err){
-    res.status(400).json({
-      error:"post getting failed",
+    res.status(401).json({
+      error:'failed to get post',
       details:err
     })
+   
   }
- 
 }
-
 
 
 
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -2,87 +2,171 @@
 import type { Post } from "../../../generated/prisma/client";
 import { prisma } from "../../lib/prisma";
 
-const getPost = async(payloads:{search : string | undefined, tags : string[] | []}) => {
+// const getPost = async(payloads:{search : string | undefined, tags : string[] | [], 
+//    isFeatured : boolean | undefined, status:postStatus | undefined,
+//   authorId:string|undefined}) => {
 
-  const andConditions = []
+//   const andConditions = []
 
-  if(payloads.search){
-    andConditions.push(   { OR:[
+//   if(payloads.search){
+//     andConditions.push(   { OR:[
 
-        {
-          title:{
-            contains:payloads.search as string
-          }
-        },
-
-
-        {
-          content:{
-            contains:payloads.search as string
-          }
-        },
+//         {
+//           title:{
+//             contains:payloads.search as string
+//           }
+//         },
 
 
-        {
-          tags:{
-            has:payloads.search as string
-          }
-        }
+//         {
+//           content:{
+//             contains:payloads.search as string
+//           }
+//         },
+
+
+//         {
+//           tags:{
+//             has:payloads.search as string
+//           }
+//         },
+
+
+      
 
 
 
-      ]})
-  }
+//       ]})
+
+// }
 
 
 
-  if(payloads.tags.length> 0){
-   andConditions.push({
-   tags:{
-        hasEvery : payloads.tags 
-      }
-   })
-  }
+//   if(payloads.tags.length> 0){
+//    andConditions.push({
+//    tags:{
+//         hasEvery : payloads.tags 
+//       }
+//    })
+//   }
 
-  const results = await prisma.post.findMany({
-    //  where: {
-    //   OR : [
-    //     {
-    //      title : {
-    //   contains : payloads.search as string,
-    //   mode:"insensitive"
-    //  }
-    //     },
 
-    //    {
-    //    content:{
-    //   contains:payloads.search as string,
-    //   mode: "insensitive"
-    //  }
-    //    },
+//    if(typeof payloads.isFeatured === 'boolean'){
+//     andConditions.push({
+//     isFeatured : payloads.isFeatured
+//     })
+//   }
 
-    //    {
-    //     tags: {
-    //       has: payloads.search as string
-    //     }
-    //    }
+
+//   if(payloads.status){
+//     andConditions.push({
+//       status: payloads.status
+//     })
+//   }
+
+
+//   if(payloads.authorId){
+//     andConditions.push({
+//       authorId:payloads.authorId
+//     })
+//   }
+
+//   const results = await prisma.post.findMany({
+//     //  where: {
+//     //   OR : [
+//     //     {
+//     //      title : {
+//     //   contains : payloads.search as string,
+//     //   mode:"insensitive"
+//     //  }
+//     //     },
+
+//     //    {
+//     //    content:{
+//     //   contains:payloads.search as string,
+//     //   mode: "insensitive"
+//     //  }
+//     //    },
+
+//     //    {
+//     //     tags: {
+//     //       has: payloads.search as string
+//     //     }
+//     //    }
 
 
    
-    //   ]
-    //  } 
+//     //   ]
+//     //  } 
 
-    where : {
+//     where : {
   
- AND: andConditions
+//  AND: andConditions
 
- }
+//  }
+//   })
+//   return results
+// }
+
+
+
+const getPost = async(payloads:{search:string|undefined, tags:string[] })=> {
+
+  const andConditions:any[] = []
+ 
+  // search with title, content
+if(payloads.search){
+  andConditions.push({
+  OR:[
+    {
+     title:{
+    contains:payloads.search,
+    mode:"insensitive"
+   }
+  },
+
+   
+    {
+      content:{
+    contains:payloads.search,
+    mode:"insensitive"
+    }
+    },
+
+
+
+    {
+      tags:{
+        has:payloads.search
+        
+      }
+    }
+
+
+  ]
+   
   })
-  return results
+
+
+  if(payloads.tags.length>0){
+   andConditions.push({
+    tags:{
+      hasEvery:payloads.tags
+    }
+   })
+  }
+
+
+
+ const results = prisma.post.findMany({
+   where:{
+    AND: andConditions
+    }
+   
+ })
+ return results
 }
-
-
-
+}
 
 
 
@@ -99,7 +183,7 @@ const createPost = async (data: Omit<Post, 'id' | 'createdAt'| 'updatedAt' | "is
 } 
 
 export const postServices = {
-  getPost ,
+    getPost,
     createPost
     
 }
